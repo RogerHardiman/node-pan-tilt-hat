065c5eb644c38b2f1855435270f15078ac501bee
Roger Hardiman <opensource@rjh.org.uk>
2018-12-28 21:34:37 +0000

    Improve error handling and allow it to run on non Pi machines

--- bin/node-pantilthat.py
+++ bin/node-pantilthat.py
@@ -9,7 +9,7 @@
 
 #  The MIT License (MIT)
 #
-#  Copyright (c) 2017 Roger Hardiman
+#  Copyright (c) 2017, 2018 Roger Hardiman
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -29,16 +29,22 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
 
-import pantilthat 
 import sys
 
+try:
+    import pantilthat
+    hat_module_loaded = True
+except ImportError:
+    hat_module_loaded = False 
+    print('ERROR: pantilthat module not installed on this computer');
+
 print('Valid commands are');
-print('pan angle');
-print('tilt angle');
-print('goto angle angle');
-print('get_pan');
-print('get_tilt');
-print('exit');
+print('  pan angle');
+print('  tilt angle');
+print('  goto angle angle');
+print('  get_pan');
+print('  get_tilt');
+print('  exit');
 
 while True:
     line = sys.stdin.readline();
@@ -47,17 +53,24 @@ while True:
     if (len(list) >= 1):
         command = list[0];
  
-        if (command == 'pan' and len(list)>=2):
-            pantilthat.pan(float(list[1]));
-        elif (command == 'tilt' and len(list)>=2):
-            pantilthat.tilt(float(list[1]));
-        elif (command == 'goto' and len(list)>=3):
-            pantilthat.pan(float(list[1]));
-            pantilthat.tilt(float(list[2]));
-        elif (command == 'get_pan'):
-            print(pantilthat.get_pan());
-        elif (command == 'get_tilt'):
-            print(pantilthat.get_tilt());
-        elif (command == 'exit'):
+        if (command == 'exit'):
             exit();
+        else:
+	    try:
+                if (command == 'pan' and len(list)>=2):
+                    pantilthat.pan(float(list[1]));
+                elif (command == 'tilt' and len(list)>=2):
+                    pantilthat.tilt(float(list[1]));
+                elif (command == 'goto' and len(list)>=3):
+                    pantilthat.pan(float(list[1]));
+                    pantilthat.tilt(float(list[2]));
+                elif (command == 'get_pan'):
+                    print(pantilthat.get_pan());
+                elif (command == 'get_tilt'):
+                    print(pantilthat.get_tilt());
+                else:
+                    print('error processing command'); 
+            except:
+                print('error processing command'); 
+
 
